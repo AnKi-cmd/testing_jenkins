1586f086f3a87fc667e93814f77267eaccfd4230
AnKi-cmd <ankiandrei@yandex.ru>
2021-07-08 23:11:50 +0200

    master

--- test_program.py
+++ test_program.py
@@ -15,7 +15,7 @@ def subtraction(a,b):
 def test_answer():
 	for i in range(1,1000):
 		for j in range(1,1000):
-			assert mult(i,j) == (i*j)
+			assert mult(i,j) == (i*j+1)
 			assert division(i,j) == (i/j)
 			assert addition(i,j) == (i+j)
 			assert subtraction(i,j) == (i-j)
